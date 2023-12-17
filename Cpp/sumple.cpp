e5075da030a3c480e59dc2e719992a4e248becff
Gen Hashimoto <85538741+zilconia@users.noreply.github.com>
2023-12-17 11:52:52 +0000

    Cpp学習

--- Cpp/sumple.cpp
+++ Cpp/sumple.cpp
@@ -1,5 +1,11 @@
 #include<iostream>
+using namespace std;
+
 int main(void){
-    std::cout << "Hello World!\n";
+    string s;
+    
+    cin >> s;
+    cout << "Hello World!\n";
+    cout << "入力内容：" << s << "\n";
     return 0;
 }
