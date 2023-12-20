1f98edf58225161e9066e9a09f8670261cbb8088
Gen Hashimoto <85538741+zilconia@users.noreply.github.com>
2023-12-20 01:58:16 +0000

    変更

--- Python/Gemini/gemini.py
+++ Python/Gemini/gemini.py
@@ -12,7 +12,7 @@ model = genai.GenerativeModel('gemini-pro')
 chat = model.start_chat()
 response = chat.send_message('どのプログラミング言語を学べば良いですか？最もオススメの言語を一つだけ教えてください。')
 print(response.text)
- 
+
 print("-" * 50)
  
 response = chat.send_message('その言語の特徴は？')
